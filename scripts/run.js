bd8bb0f706a0bdc4e519b6bc6143f02730f52d32
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-05-14 18:13:08 +1000

    Add runner script

---
+++ scripts/run.js
@@ -0,0 +1,56 @@
+
+const ethers = require("ethers");
+
+const main = async () => {
+    //Get the contract and deploy it
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    let randomBalance = await hre.ethers.provider.getBalance(randomPerson.address);
+    
+
+    console.log("Owner balance: " + ownerBalance);
+    console.log("Other balance" + randomBalance);
+
+    const pifFactory = await hre.ethers.getContractFactory('PayItForward');
+    const pif = await pifFactory.deploy({value: hre.ethers.utils.parseEther('0.5')});
+    await pif.deployed();
+
+    let contractBalance = await hre.ethers.provider.getBalance(pif.address);
+    console.log('Contract balance: ' + contractBalance);
+
+    console.log("Contract deployed to:", pif.address);
+  
+    let txn = await pif.connect(randomPerson).payItForward({value: hre.ethers.utils.parseEther('0.1')});
+    await txn.wait();
+
+    ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    randomBalance = await hre.ethers.provider.getBalance(randomPerson.address);
+    contractBalance = await hre.ethers.provider.getBalance(pif.address);
+
+    console.log("Owner balance: " + ownerBalance);
+    console.log("Other balance" + randomBalance);
+    console.log('Contract balance: ' + contractBalance);
+
+    txn = await pif.payItForward({value: hre.ethers.utils.parseEther('0.6')});
+    await txn.wait();
+
+    ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    randomBalance = await hre.ethers.provider.getBalance(randomPerson.address);
+    contractBalance = await hre.ethers.provider.getBalance(pif.address);
+
+    console.log("Owner balance: " + ownerBalance);
+    console.log("Other balance" + randomBalance);
+    console.log('Contract balance: ' + contractBalance);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
